03da621f305695eb154656d0ea743b9156e6768b
Alex Gillis <alex@xander.com>
2022-06-23 07:23:07 -0400

    removed css from parent component to child, remove default search term

--- src/SupeInfo.svelte
+++ src/SupeInfo.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
    import { onMount } from 'svelte';
-   import Search from "./Search.svelte";
    import Supe from "./Supe.svelte"
 
    let supeinfo = [];
@@ -24,18 +23,7 @@
    <div id="cards_display">
       <input type="search" bind:value={search} class="ms-auto w-auto" placeholder="Search" />
    {#each visibleSupes as supe}
-      <div class="supe_flex_box" >
-         <div>
-            <img src="{supe.images.sm}" alt="Image of {supe.name}"/>
-         </div>
-         <div class="supe_card_content">
-            <h1>{supe.name}</h1>
-            <p>Fullname: {supe.biography.fullName}</p>
-            <p>Race: {supe.appearance.race}</p>
-            <p>Alignment: {supe.biography.alignment}</p>
-            <p>Publisher: {supe.biography.publisher}</p>
-         </div>
-      </div>
+      <Supe {supe} />
    {/each}
    </div>
    </div>
@@ -62,33 +50,5 @@
       overflow: hidden;
       max-height: 900px;
       border-radius: 20px;
-   }
-
-   .supe_card_container {
-      max-height: 180px;
-      width: 500px;
-      border-radius: 10px;
-      text-align: left;
-      /*margin-bottom: 1em;*/
-      /*padding: 5px 5px 5px 0px;*/
-   }
-   .supe_card_content {
-      /*margin-left: 10px;*/
-      max-height: 100px;
-      text-align: left;
-      border: 1px #666666;
-      border-radius: 20px;
-   }
-
-   img {
-      height: 90%;
-      border-radius: 10px 0px 0px 10px;
-      /*max-width: 50%;*/
-      /*max-height: 70%;*/
-   }
-   .supe_flex_box {
-      display: flex;
-      flex-direction: row;
-      height: 250px;
    }
 </style>
