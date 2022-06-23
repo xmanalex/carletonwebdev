07e6315f82f3142e8ce77c74319bae04426db967
Alex Gillis <alex@xander.com>
2022-06-23 13:19:50 -0400

    add css  border on super information, seperate flex css for image, supe content

--- src/Supe.svelte
+++ src/Supe.svelte
@@ -4,16 +4,19 @@
 
 <main>
     <div class="supe_flex_box" >
-        <div>
+        <div id="supe_image">
             <img src="{supe.images.sm}" alt="Image of {supe.name}"/>
         </div>
-        <div class="supe_card_content">
+        <div id="supe_general_info" class="supe_card_content">
+            <div class="supe_general_info_spacer">
             <h1>{supe.name}</h1>
             <p>Fullname: {supe.biography.fullName}</p>
             <p>Race: {supe.appearance.race}</p>
             <p>Alignment: {supe.biography.alignment}</p>
             <p>Publisher: {supe.biography.publisher}</p>
+            </div>
         </div>
+        <span>+</span>
     </div>
 </main>
 
@@ -21,12 +24,44 @@
     .supe_flex_box {
         display: flex;
         flex-direction: row;
-        justify-content: space-evenly;
+        /*justify-content: center;*/
+
+    }
+
+    .supe_flex_box > #supe_image {
+        text-align: right;
+        max-height: 220px;
+    }
+
+    .supe_flex_box > #supe_general_info {
+        /*width: 100px;*/
+        text-align: left;
+        /*line-height: 60px;*/
+        font-size: 12px;
+        width: 300px;
+        height: 199px;
+        border: 1px solid lightgrey;
+        border-left-style: none;
+        box-sizing: border-box;
+        border-radius: 0px 10px 10px 0px;align-self: flex-start;
+    }
+
+    .supe_flex_box > span {
+        /*width: 100px;*/
+        /*margin: 10px;*/
+        text-align: right;
+        /*line-height: 60px;*/
+        font-size: 12px;
+        max-height: 220px;
     }
     .supe_card_content {
         text-align: left;
         border: 1px #666666;
         border-radius: 20px;
+    }
+
+    .supe_general_info_spacer {
+        margin-left: 10px;
     }
     .power_stats_flex_box {
         display: flex;
